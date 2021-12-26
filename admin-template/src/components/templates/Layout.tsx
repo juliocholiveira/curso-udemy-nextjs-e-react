b41322d347e051d65185596552e751abc52ba1a6
Júlio César <juliocholiveira@gmail.com>
2021-12-26 16:03:09 -0300

    feat: adiciona novas configurações de layout

--- admin-template/src/components/templates/Layout.tsx
+++ admin-template/src/components/templates/Layout.tsx
@@ -1,21 +1,21 @@
-import Cabecalho from "./Cabecalho";
-import Conteudo from "./Conteudo";
-import MenuLateral from "./MenuLateral";
+import Cabecalho from './Cabecalho';
+import Conteudo from './Conteudo';
+import MenuLateral from './MenuLateral';
 
 interface LayoutProps {
-  titulo: string
-  subtitulo: string
-  children?: any
+  titulo: string;
+  subtitulo: string;
+  children?: any;
 }
 
-export default function Layout(props: LayoutProps){
-  return(
-    <div>
+export default function Layout(props: LayoutProps) {
+  return (
+    <div className="flex h-screen w-screen">
       <MenuLateral />
-      <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-      <Conteudo>
-        {props.children}
-      </Conteudo>
+      <div className={`flex flex-col bg-gray-400 dark:bg-gray-800 p-5 w-full`}>
+        <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+        <Conteudo>{props.children}</Conteudo>
+      </div>
     </div>
-  )
+  );
 }
--- admin-template/src/pages/index.tsx
+++ admin-template/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/templates/Layout";
 
 export default function Home() {
   return (
-    <div className={`flex justify-center items-center h-screen bg-gradient-to-r from-green-500 to-blue-500`}>
+    <div className={``}>
       <Layout titulo="Página Inicial" subtitulo="Estamos construindo um template">
         <h1>Conteúdo!!!</h1>
       </Layout>
