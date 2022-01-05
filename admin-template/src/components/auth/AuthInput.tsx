1f17993eb3eec47b2c928c3e3bec618aefa3af37
Julio Oliveira <juliocholiveira@gmail.com>
2022-01-04 22:48:16 -0300

    feat: estiliza a página de autenticação

--- admin-template/src/components/auth/AuthInput.tsx
+++ admin-template/src/components/auth/AuthInput.tsx
@@ -9,13 +9,17 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
   return props.visivel ? (
-    <div>
+    <div className="flex flex-col mt-2">
       <label htmlFor="">{props.label}</label>
       <input
         type={props.tipo || 'text'}
         value={props.valor}
         onChange={(e) => props.onChangeMe(e.target.value)}
         required={props.obrigatorio}
+        className={`
+          bg-gray-200 w-full px-2 py-2 rounded-lg mt-2 border 
+          focus:bg-white focus:outline-none focus:border-blue-500
+        `}
       />
     </div>
   ) : (
--- admin-template/src/pages/authentication.tsx
+++ admin-template/src/pages/authentication.tsx
@@ -6,23 +6,56 @@ export default function Authentication() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
+  const submeter = () => {
+    if (modo === 'login') console.log('login');
+    else console.log('cadastrar');
+  };
+
   return (
-    <div>
-      <AuthInput
-        label="Email"
-        valor={email}
-        obrigatorio={true}
-        visivel={true}
-        onChangeMe={setEmail}
-      />
-      <AuthInput
-        label="Senha"
-        tipo="password"
-        valor={senha}
-        obrigatorio={true}
-        visivel={true}
-        onChangeMe={setSenha}
-      />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-1/3">
+        <h1 className={`text-xl font-bold mb-5`}>
+          {modo === 'login'
+            ? 'Entre com sua conta'
+            : 'Cadastre-se na plataforma'}
+        </h1>
+        <AuthInput
+          label="Email"
+          valor={email}
+          obrigatorio={true}
+          visivel={true}
+          onChangeMe={setEmail}
+        />
+        <AuthInput
+          label="Senha"
+          tipo="password"
+          valor={senha}
+          obrigatorio={true}
+          visivel={true}
+          onChangeMe={setSenha}
+        />
+        <button
+          onClick={submeter}
+          className={`
+            w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg
+            px-4 py-3 mt-6 
+
+        `}
+        >
+          {modo === 'login' ? 'Login' : 'Cadastrar'}
+        </button>
+        <hr className="my-6" />
+        <button
+          onClick={submeter}
+          className={`
+            w-full bg-red-500 hover:bg-red-400 text-white rounded-lg
+            px-4 py-3
+
+        `}
+        >
+          Login com o Google
+        </button>
+      </div>
     </div>
   );
 }
