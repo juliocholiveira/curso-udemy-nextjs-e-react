78204be4974b524d2f6ac259e96d43862afd2b0c
Júlio César <juliocholiveira@gmail.com>
2022-01-17 07:15:18 -0300

    fix: ajusta avatar para usuário null

--- admin-template/src/components/templates/Avatar.tsx
+++ admin-template/src/components/templates/Avatar.tsx
@@ -6,11 +6,17 @@ export default function Avatar() {
   return (
     <div>
       <Link href="/profile">
-        <img
-          className="w-10 h-10 ml-2 rounded-full cursor-pointer"
-          src={usuario?.imagemUrl}
-          alt="Avatar"
-        />
+        {usuario ? (
+          <img
+            className="w-10 h-10 ml-2 rounded-full cursor-pointer"
+            src={usuario?.imagemUrl}
+            alt="Avatar"
+          />
+        ) : (
+          <div className="w-10 h-10 ml-2 bg-red-700 rounded-full cursor-pointer flex justify-center items-center">
+            <span className="text-lg text-white font-semibold">A</span>
+          </div>
+        )}
       </Link>
     </div>
   );
