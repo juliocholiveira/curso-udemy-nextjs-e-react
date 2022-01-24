aa03ebcff46136b042a34e2a7136ebdbe06dea92
Julio Oliveira <juliocholiveira@gmail.com>
2022-01-24 07:08:07 -0300

    feat: salva tema no localStorage

--- admin-template/src/data/context/AppContext.tsx
+++ admin-template/src/data/context/AppContext.tsx
@@ -1,20 +1,28 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 type Tema = 'dark' | '';
 
 interface AppContextProps {
-  tema?: Tema;
+  tema?: string;
   alternarTema?: () => void;
 }
 
 const AppContex = createContext<AppContextProps>({});
 
 export function AppProvider(props) {
-  const [tema, setTema] = useState<Tema>('');
+  const [tema, setTema] = useState('');
 
   const alternarTema = () => {
-    setTema(tema == 'dark' ? '' : 'dark');
+    const novoTema = tema == 'dark' ? '' : 'dark';
+    setTema(novoTema);
+    localStorage.setItem('tema', novoTema);
   };
+
+  useEffect(() => {
+    const temaSalvo = localStorage.getItem('tema');
+    setTema(temaSalvo);
+  }, []);
+
   return (
     <AppContex.Provider
       value={{
