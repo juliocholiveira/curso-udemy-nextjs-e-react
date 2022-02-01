045d7a8927eca4b1a5b7dc365b025899395270fb
Júlio César <juliocholiveira@gmail.com>
2022-02-01 05:57:16 -0300

    feat: adiciona botões de Editar e Excuir

--- admin-template/src/components/Tabela.tsx
+++ admin-template/src/components/Tabela.tsx
@@ -1,27 +1,24 @@
 import Cliente from '../core/Cliente';
+import { IconEdit, IconDelete } from '../components/icons';
 
 interface TabelaProps {
   clientes?: Cliente[];
+  clienteSelecionado?: (cliente: Cliente) => void;
+  clienteExcluido?: (cliente: Cliente) => void;
 }
 
 export default function Tabela(props: TabelaProps) {
-  const clientes = [
-    new Cliente('Júlio', 40, '1'),
-    new Cliente('Janaina', 37, '2'),
-    new Cliente('Miguel', 8, '3'),
-    new Cliente('Otávio', 5, '4'),
-  ];
-
   const renderizarCabecalho = () => (
     <tr>
       <th className="text-left p-3">Id</th>
       <th className="text-left p-3">Nome</th>
       <th className="text-left p-3">Idade</th>
+      <th className="p-3">Ações</th>
     </tr>
   );
 
   const renderizarDados = () =>
-    clientes?.map((cliente, i) => {
+    props.clientes?.map((cliente, i) => {
       return (
         <tr
           key={cliente.id}
@@ -32,9 +29,37 @@ export default function Tabela(props: TabelaProps) {
           <td className="text-left p-3">{cliente.id}</td>
           <td className="text-left p-3">{cliente.nome}</td>
           <td className="text-left p-3">{cliente.idade}</td>
+          {renderizarAcoes(cliente)}
         </tr>
       );
     });
+
+  const renderizarAcoes = (cliente: Cliente) => {
+    return (
+      <td className="flex justify-center">
+        <button
+          onClick={() => props.clienteSelecionado?.(cliente)}
+          className={`
+          flex justify-center items-center
+          text-green-600 p-2 rounded-full m-1
+          hover:bg-purple-50
+        `}
+        >
+          {IconEdit}
+        </button>
+        <button
+          onClick={() => props.clienteExcluido?.(cliente)}
+          className={`
+          flex justify-center items-center
+          text-red-600 p-2 rounded-full m-1
+          hover:bg-purple-50
+        `}
+        >
+          {IconDelete}
+        </button>
+      </td>
+    );
+  };
 
   return (
     <table className="w-full rounded-xl overflow-hidden">
--- admin-template/src/pages/crud.tsx
+++ admin-template/src/pages/crud.tsx
@@ -1,12 +1,32 @@
 import Tabela from '../components/Tabela';
 import Layout from '../components/templates/Layout';
+import Cliente from '../core/Cliente';
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Júlio', 40, '1'),
+    new Cliente('Janaina', 37, '2'),
+    new Cliente('Miguel', 8, '3'),
+    new Cliente('Otávio', 5, '4'),
+  ];
+
+  const clienteSelecionado = (cliente: Cliente) => {
+    alert(`Alterando... ${cliente.nome}`);
+  };
+
+  const clienteExcluido = (cliente: Cliente) => {
+    alert(`Exclindo... ${cliente.nome}`);
+  };
+
   return (
     <div className={``}>
       <Layout titulo="Página CRUD" subtitulo="Estamos construindo um CRUD">
         <h1>Cadastro de Clientes</h1>
-        <Tabela />
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
       </Layout>
     </div>
   );
